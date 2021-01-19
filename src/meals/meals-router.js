bd40964d9a49e6f3359457f8040edd30fffb0faf
yefenny <yefennychireno@Yefennys-MacBook-Pro.local>
2021-01-19 14:58:45 -0500

    add get meal by id

--- src/meals/meals-router.js
+++ src/meals/meals-router.js
@@ -14,6 +14,19 @@ mealsRouter.route('/').get((req, res, next) => {
     .catch(next);
 });
 
+mealsRouter.route('/:id').get((req, res, next) => {
+  const { id } = req.params;
+  const userId = req.user.id;
+
+  MealsService.getMealById(req.app.get('db'), id, userId)
+    .then((meal) => {
+      if (meal) {
+        res.status(200).json(meal);
+      }
+    })
+    .catch(next);
+});
+
 mealsRouter.route('/find/:query').get((req, res, next) => {
   const { query } = req.params;
   const { id } = req.user;
--- src/meals/meals-service.js
+++ src/meals/meals-service.js
@@ -5,6 +5,9 @@ const MealsService = {
   getFilteredMeals(db, parameter, userId) {
     const query = `select * from meals where user_id = ${userId} and( title ilike '%${parameter}%' or description ilike '%${parameter}%')`;
     return db.raw(query).then((res) => res.rows);
+  },
+  getMealById(db, id, userId) {
+    return db('meals').select('*').where({ user_id: userId, id: id }).first();
   }
 };
 module.exports = MealsService;
