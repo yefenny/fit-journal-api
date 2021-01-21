6eb06557507233733d5eaa93f8efcf6ccab37ad9
yefenny <yefennychireno@Yefennys-MacBook-Pro.local>
2021-01-21 14:36:17 -0500

    add post route to body-composition

--- src/body-composition/body-compositions-router.js
+++ src/body-composition/body-compositions-router.js
@@ -2,13 +2,114 @@ const express = require('express');
 const BodyCompositionsService = require('./body-compositions-service');
 const bodyCompositionsRouter = express.Router();
 
-bodyCompositionsRouter.route('/').get((req, res, next) => {
-  const { id } = req.user;
+bodyCompositionsRouter
+  .route('/')
+  .get((req, res, next) => {
+    const { id } = req.user;
 
-  BodyCompositionsService.getAllBodyCompositions(req.app.get('db'), id)
-    .then((values) => {
-      if (values) res.send(values);
+    BodyCompositionsService.getAllBodyCompositions(req.app.get('db'), id)
+      .then((values) => {
+        if (values) res.send(values);
+      })
+      .catch(next);
+  })
+  .all(validateBodyComposition)
+  .post((req, res, next) => {
+    const {
+      left_arm,
+      right_arm,
+      chest,
+      waist,
+      hips,
+      left_thigh,
+      right_thigh,
+      left_calf,
+      right_calf,
+      weight,
+      body_fat
+    } = req.body;
+    const checkValues = {
+      left_arm,
+      right_arm,
+      chest,
+      waist,
+      hips,
+      left_thigh,
+      right_thigh,
+      left_calf,
+      right_calf,
+      body_fat
+    };
+    const userId = req.user.id;
+    const newEntry = { weight, user_id: userId };
+    const db = req.app.get('db');
+
+    for (let [key, value] of Object.entries(checkValues)) {
+      if (value) newEntry[key] = value;
+    }
+
+    BodyCompositionsService.createBodyComposition(db, newEntry)
+      .then((val) => {
+        if (val) return res.json(val);
+      })
+      .catch(next);
+  });
+
+bodyCompositionsRouter.route('/:id').get((req, res, next) => {
+  const { id } = req.params;
+  const userId = req.user.id;
+  const db = req.app.get('db');
+
+  BodyCompositionsService.getBodyCompositionById(db, id, userId)
+    .then((val) => {
+      if (val) return res.status(200).json(val);
     })
     .catch(next);
 });
+
+function validateBodyComposition(req, res, next) {
+  const {
+    left_arm,
+    right_arm,
+    chest,
+    waist,
+    hips,
+    left_thigh,
+    right_thigh,
+    left_calf,
+    right_calf,
+    weight,
+    body_fat
+  } = req.body;
+
+  const shouldBeNumber = {
+    left_arm,
+    right_arm,
+    chest,
+    waist,
+    hips,
+    left_thigh,
+    right_thigh,
+    left_calf,
+    right_calf,
+    weight,
+    body_fat
+  };
+
+  if (!weight) {
+    return res
+      .status(400)
+      .json({ error: { message: 'Weight value is required' } });
+  }
+
+  for (let [key, value] of Object.entries(shouldBeNumber)) {
+    if (value) {
+      if (isNaN(value))
+        return res
+          .status(400)
+          .json({ error: { message: `${key} value should be a number` } });
+    }
+  }
+  next();
+}
 module.exports = bodyCompositionsRouter;
--- src/body-composition/body-compositions-service.js
+++ src/body-composition/body-compositions-service.js
@@ -4,6 +4,27 @@ const BodyCompositionsService = {
       .select('*')
       .where('user_id', userId)
       .orderBy('date_created', 'desc');
+  },
+  getBodyCompositionById(db, id, userId) {
+    return db('users_body_composition')
+      .where({ id: id, user_id: userId })
+      .first();
+  },
+  deleteBodyComposition(db, id, userId) {
+    return db('users_body_composition')
+      .where({ id: id, user_id: userId })
+      .delete();
+  },
+  createBodyComposition(db, values) {
+    return db('users_body_composition')
+      .insert(values)
+      .returning('*')
+      .then((rows) => rows[0]);
+  },
+  updateBodyComposition(db, id, userId, values) {
+    return db('users_body_composition')
+      .where({ id: id, user_id: userId })
+      .update(values);
   }
 };
 
