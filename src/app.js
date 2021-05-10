85f8c341b367b14c43e596ddaea500d91545f0f9
Yefenny Chireno <yefenny300@icloud.com>
2021-05-09 22:27:23 -0400

    change cors options

--- src/app.js
+++ src/app.js
@@ -17,45 +17,15 @@ const app = express();
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
-var corsOptions = {
-  origin: 'https://fit-journal-client-yefenny.vercel.app',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
 app.use(morgan(morganOption));
-app.use(cors(corsOptions));
+app.use(cors({ origin: null }));
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+// app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://fit-journal-client-yefenny.vercel.app'
-  );
-
-  // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 app.use(express.static('public'));
 app.use(jsonParser);
 
-app.use('/api/users', cors(corsOptions), userRouter);
+app.use('/api/users', userRouter);
 app.use(auth);
 app.use('/api/exercises', exercisesRouter);
 app.use('/api/body-parts', bodyPartsRouter);
