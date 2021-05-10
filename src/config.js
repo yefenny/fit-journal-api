be6755fb1e34d7b32dae6dc91d22730657df9ec6
Yefenny Chireno <yefenny300@icloud.com>
2021-05-09 23:51:29 -0400

    change config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
+const pg = require('pg');
+pg.defaults.ssl =
+  process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false;
+
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  JWT_SECRET: process.env.JWT_SECRET || 'my-saved-secret'
+  DATABASE_URL:
+    process.env.DATABASE_URL || 'postgresql://USERNAME@localhost/DATABASE_NAME',
 };
