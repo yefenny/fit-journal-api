5a3645a32f0b4d7920b98eeb142d3a6e714d6c51
Yefenny Chireno <yefenny300@icloud.com>
2021-05-09 23:23:34 -0400

    change cors options

--- src/users/user-router.js
+++ src/users/user-router.js
@@ -2,40 +2,46 @@ const express = require('express');
 const auth = require('../middleware/auth');
 const userRouter = express.Router();
 const UsersService = require('./users-service');
-userRouter.route('/signup/').post((req, res, next) => {
-  const { email, full_name, password } = req.body;
-  const db = req.app.get('db');
-  const requiredFields = { email, full_name, password };
-  for (let [key, value] of Object.entries(requiredFields)) {
-    if (!value) {
-      return res
-        .status(400)
-        .json({ error: { message: `'${key}' is missing` } });
-    }
-  }
-
-  UsersService.getUserByEmail(db, email)
-    .then((user) => {
-      if (user) {
-        return res
-          .status(400)
-          .json({ error: { message: `email already exists` } });
+var cors = require('cors');
+userRouter
+  .route('/signup/')
+  .post(
+    cors({ origin: 'https://fit-journal-client-yefenny.vercel.app' }),
+    (req, res, next) => {
+      const { email, full_name, password } = req.body;
+      const db = req.app.get('db');
+      const requiredFields = { email, full_name, password };
+      for (let [key, value] of Object.entries(requiredFields)) {
+        if (!value) {
+          return res
+            .status(400)
+            .json({ error: { message: `'${key}' is missing` } });
+        }
       }
-      requiredFields.password = UsersService.bcryptPassword(password);
 
-      UsersService.insertUser(req.app.get('db'), requiredFields)
-        .then(() => {
-          UsersService.getUserByEmail(db, email).then((user) => {
-            if (user) {
-              const token = UsersService.generateJwtToken(user);
-              return res.status(201).send({ authToken: token });
-            }
-          });
+      UsersService.getUserByEmail(db, email)
+        .then((user) => {
+          if (user) {
+            return res
+              .status(400)
+              .json({ error: { message: `email already exists` } });
+          }
+          requiredFields.password = UsersService.bcryptPassword(password);
+
+          UsersService.insertUser(req.app.get('db'), requiredFields)
+            .then(() => {
+              UsersService.getUserByEmail(db, email).then((user) => {
+                if (user) {
+                  const token = UsersService.generateJwtToken(user);
+                  return res.status(201).send({ authToken: token });
+                }
+              });
+            })
+            .catch(next);
         })
         .catch(next);
-    })
-    .catch(next);
-});
+    }
+  );
 
 userRouter.route('/login/').post((req, res, next) => {
   const { email, password } = req.body;
